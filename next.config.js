1f4fa060d6f03d4d5e0bef5fd8dcef82e0c63be8
Joel Jarro <59368787+Quirrod@users.noreply.github.com>
2023-07-02 12:44:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
     reactStrictMode: false,
     ignoreBuildErrors: true,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
 }
 
 module.exports = nextConfig
